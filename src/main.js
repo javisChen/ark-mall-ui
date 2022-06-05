835c945f9de88b0f98cf00073a0af1a084d4d9b1
Javis <javischen9548@foxmail.com>
2022-06-05 21:21:27 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import './all.css'
 
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
 const app = createApp(App);
 app.use(router)
-app.mount('#app')
+    .mount('#app')
--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
 
 import CommonTopBar from "../common/CommonTopBar.vue";
-import {reactive, toRefs} from 'vue';
+import {reactive, toRefs, defineComponent} from 'vue';
 import {useRoute, useRouter} from 'vue-router';
+import {NCheckbox} from 'naive-ui'
 
 const data = reactive({
-  product: {}
+  cartList: [
+    {
+      id: 1,
+      name: 'Redmi Note 11T Pro  6GB+128GB 子夜黑',
+      picUrl: '@/assets/img/seckill/shop/t.jpg',
+      price: 1699,
+      quantity: 1,
+      subTotal: 1699
+    },
+    {
+      id: 1,
+      name: 'Redmi Note 11T Pro  6GB+128GB 子夜黑',
+      picUrl: '@/assets/img/seckill/shop/t.jpg',
+      price: 1699,
+      quantity: 1,
+      subTotal: 1699
+    },
+  ]
 })
 
 const {
-  product,
+  cartList,
 } = toRefs(data)
 
 
@@ -30,14 +48,45 @@ const {
   <div class="main">
     <div class="container">
       <div class="cart-table">
-        <div class="cart-table-row">
-          <div class="cart-table-col">全选</div>
-          <div class="cart-table-col"></div>
-          <div class="cart-table-col">商品名称</div>
-          <div class="cart-table-col">单价</div>
-          <div class="cart-table-col">数量</div>
-          <div class="cart-table-col">小计</div>
-          <div class="cart-table-col">操作</div>
+        <div class="cart-table-header">
+          <div class="cart-table-row">
+            <div class="cart-table-col check">
+              <n-checkbox class="checkbox" size="large" label=""/>
+              &nbsp;&nbsp;全选
+            </div>
+            <div class="cart-table-col picture"></div>
+            <div class="cart-table-col product-name">商品名称</div>
+            <div class="cart-table-col price">单价</div>
+            <div class="cart-table-col num">数量</div>
+            <div class="cart-table-col total">小计</div>
+            <div class="cart-table-col">操作</div>
+          </div>
+        </div>
+        <div class="cart-table-body">
+          <div class="cart-table-row" v-for="item in cartList">
+            <div class="cart-table-col check">
+              <n-checkbox size="large" label=""/>
+            </div>
+            <div class="cart-table-col picture">
+              <img src="../../assets/test-prod.webp" alt="">
+            </div>
+            <div class="cart-table-col product-name">{{ item.name }}</div>
+            <div class="cart-table-col price">{{ item.price }}元</div>
+            <div class="cart-table-col num">{{ item.quantity }}</div>
+            <div class="cart-table-col total">{{ item.subTotal }}元</div>
+            <div class="cart-table-col">删除</div>
+          </div>
+        </div>
+      </div>
+      <div class="cart-bar">
+        <div class="continue">继续购物</div>
+        <div class="selected">
+          已选择
+          <span class="quantity primary">1</span> 件
+        </div>
+        <div class="total-price primary">合计：<span class="money">0</span>元</div>
+        <div class="btn-box">
+          <button class="settle primary-bg">去结算</button>
         </div>
       </div>
     </div>
@@ -53,6 +102,7 @@ const {
   flex-direction: row;
   justify-content: center;
 }
+
 .header .container {
   width: 1226px;
   padding-top: 25px;
@@ -70,6 +120,7 @@ const {
   width: 48px;
   height: 48px;
 }
+
 .header-title {
   font-size: 28px;
   font-weight: 400;
@@ -91,7 +142,153 @@ const {
 
 .main .container {
   width: 1226px;
+}
+
+.cart-table {
   background-color: #FFFFFF;
+}
+
+.cart-table .cart-table-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.cart-table .cart-table-header {
+  border-bottom: 1px solid #e0e0e0
+}
+
+.cart-table .cart-table-body {
+
+}
+
+.cart-table .cart-table-row .cart-table-col {
+  width: 100px;
+  height: 70px;
+  line-height: 70px;
+  text-align: center;
+}
+
+.cart-table .cart-table-row .cart-table-col.num {
+  width: 150px;
+}
+
+.cart-table .cart-table-row .cart-table-col.total {
+  width: 201px;
+}
+
+.cart-table .cart-table-row .cart-table-col.price {
+  width: 158px;
+}
+
+.cart-table .cart-table-row .cart-table-col.product-name {
+  width: 380px;
+  text-align: left;
+  font-size: 18px;
+}
+
+.cart-table-body .cart-table-row .cart-table-col.price {
+  color: #ff6700;
+}
+
+.cart-table-body .cart-table-row  {
+  font-size: 16px;
+}
+
+.cart-table-body .cart-table-row .cart-table-col.total {
+  color: #ff6700;
+}
+
+.cart-table .cart-table-row .cart-table-col.picture {
+  width: 100px;
+}
+
+.cart-table .cart-table-row .cart-table-col.picture img {
+  width: 85px;
+  height: 85px;
+}
+
+.cart-table .cart-table-body .picture {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.cart-table .cart-table-row .cart-table-col.check {
+  width: 80px;
+  float: left;
+  text-align: left;
+  padding-left: 20px;
+}
+
+.cart-table-body .cart-table-row {
+  height: 86px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e0e0e0;
+}
+
+.cart-table-body .cart-table-col {
+  /*border: 1px solid red;*/
+}
+
+.cart-table-body .cart-table-row:last-child {
+  border-bottom: 0
+}
+
+.checkbox {
+  margin-right: 10px;
+}
+
+.cart-bar {
+  display: flex;
+  flex-direction: row;
+  height: 50px;
+  margin-top: 25px;
+  background-color: #FFFFFF;
+  color: #757575;
+  font-size: 14px;
+  align-items: center;
+  width: 100%;
+  position: relative;
+}
+
+.cart-bar .continue {
+  margin-left: 32px;
+  padding-right: 16px;
+  border-right: 1px solid #eee;
+}
+
+.cart-bar .selected {
+  margin-left: 16px;
+}
+
+.cart-bar .selected .quantity {
+
+}
+
+.cart-bar .total-price {
+  margin-left: auto;
+}
+
+.cart-bar .btn-box {
+  height: 100%;
+  border: 1px solid red;
+  margin-left: 50px;
+}
+
+.cart-bar .btn-box .settle {
+  width: 200px;
+  outline: none;
+  border: 0;
+  height: 100%;
+  color: #FFFFFF;
+  font-size: 16px;
+}
+
+.cart-bar .total-price .money {
+  font-size: 30px;
 }
 
 </style>
