d0fb9d3422b202bfe1f71af5639a7c6cabaf4ff5
Javis <javischen9548@foxmail.com>
2024-01-11 18:26:56 +0800

    u

--- src/views/shop/Search.vue
+++ src/views/shop/Search.vue
@@ -35,8 +35,18 @@ const {
 <template>
   <common-top-bar/>
   <shop-nav/>
-  <div class="home">
-    <div class="container">
+  <div class="search-container">
+    <div class="loading" style="display: none;">
+      <div class="loader"></div>
+    </div>
+    <div class="container-main" style="">
+      <div class="breadcrumbs">
+        <div class="container">
+          <a href="//www.mi.com">首页</a><span
+            class="sep">&gt;</span><a
+            href="//www.mi.com/shop/search?keyword=xiaomimix系列">全部结果</a>
+        </div>
+      </div>
       <search-filter/>
     </div>
   </div>
@@ -44,95 +54,40 @@ const {
 
 <style scoped>
 
-.home {
-  min-height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  align-items: center;
-}
 
-.home .container {
-  background-color: #e0e0e0;
+.search-container {
   width: 100%;
 }
 
-.content {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: center;
+.search-container .container-main {
+  position: relative;
 }
 
-.content .box {
-  width: 1226px;
-}
-
-.content .box .box-bd {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-
-.box-bd .item {
-  width: 230px;
-  margin-right: 14px;
-  margin-bottom: 14px;
-  background-color: #FFFFFF;
-}
-
-.box-bd .item:first-child {
-  margin-left: 0;
-}
-
-.product-hover {
-  box-shadow: 1px 4px 20px #33333359;
-  transition: all .20s ease-in;
-}
-
-.box .box-hd .title {
-  font-weight: 330;
-  line-height: 58px;
-  font-size: 22px;
-}
-
-.box .box-bd {
-  /*border: 1px solid red;*/
-  /*flex-direction: row;*/
-}
-
-.item .item-a-tag {
-  padding: 25px;
-  text-decoration: none;
-  text-align: center;
-}
-
-.item .product-img {
-  margin-bottom: 14px;
-}
-
-.item .title {
-  font-size: 14px;
-  font-weight: 400;
-  color: #333;
-  margin-bottom: 6px;
-}
-
-.item .desc {
-  height: 18px;
+.search-container .container-main .breadcrumbs {
+  height: 40px;
   font-size: 12px;
+  line-height: 40px;
+  background: #f5f5f5;
+  color: #757575;
+}
+
+.search-container a {
+  cursor: pointer;
+}
+
+.breadcrumbs a {
+  color: #757575;
+}
+
+.search-container .container-main .breadcrumbs .container {
+  width: 1226px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.search-container .container-main .breadcrumbs .sep {
+  margin: 0 0.5em;
   color: #b0b0b0;
-  margin-bottom: 5px;
-}
-
-.item .price {
-  text-align: center;
-  color: #ff6700;
-}
-
-.category-product-item-title {
-  color: #000000;
 }
 
 </style>
---
+++ src/views/shop/ShowMore.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="data.length >= 12"
+       class="more-btn"
+       @click="showMore = !showMore">更多
+    <n-icon>
+      <md-arrow-dropdown v-if="!showMore"/>
+      <md-arrow-dropup v-else/>
+    </n-icon>
+  </div>
+</template>
+<script setup>
+defineProps({
+  showMore: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  data: {
+    type: Array,
+    required: true
+  },
+})
+
+import {MdArrowDropdown, MdArrowDropup} from '@vicons/ionicons4'
+
+</script>
+
+<style scoped>
+
+</style>
